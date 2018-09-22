80d3953c0c333898fad4257ebf81f964aa7b8089
Kenny Lehr <kenman9009@gmail.com>
2018-09-22 17:52:33 -0400

    added Image component and fetch for images

---
+++ src/container/Widget.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from '../component/Image'
+
+class Widget extends React.Component{
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Widget
