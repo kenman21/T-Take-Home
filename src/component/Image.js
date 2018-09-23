8d5c1ae44e49ef02aa67cb47e08f53ac0b71d3d3
Kenny Lehr <kenman9009@gmail.com>
2018-09-23 16:27:03 -0400

    checks for user country and changes header accordingly

--- src/component/Image.js
+++ src/component/Image.js
@@ -7,14 +7,14 @@ class Image extends React.Component{
     return(
       <div className="image">
         <img className="thumbnail" src = {thumbnail}/>
-        <h1 className="title">
-          <a className="link" href={this.props.content.url}>
+        <h1 className="image-title">
+          <a className="image-link" href={this.props.content.url}>
             {this.props.content.name}
           </a>
         </h1>
-        <h1 className="branding">
-          <a className="link" href={this.props.content.url}>
-            {this.props.content.branding}
+        <h1 className="image-branding">
+          <a className="image-link" href={this.props.content.url}>
+            {this.props.content.categories ? this.props.content.categories[0][0].toUpperCase() + this.props.content.categories[0].substr(1):null} || {this.props.content.branding}
           </a>
         </h1>
       </div>
